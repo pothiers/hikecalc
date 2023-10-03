2752dc85e24d276837245c13c6d5c8539a27a206
Steve Pothier <pothiers@gmail.com>
2023-10-03 08:52:17 -0700

    all smoke pass

--- tests/smoke/smoke.sh
+++ tests/smoke/smoke.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # PURPOSE:    Wrapper for smoke test
+# EXAMPLE:
+#   ./tests/smoke/smoke.sh
 
 file=$0
 dir=`dirname $file`
@@ -13,48 +15,73 @@ SMOKEOUT="README-smoke-results.txt"
 CLIOUT="session.in"
 DISTOUT="catalina-dist.csv"
 
+export CD=~/sandbox/hikecalc/data/catalina.dat
+
 
 echo ""
 echo "Starting tests in \"$dir\" ..."
 echo ""
 echo ""
 
-cat > $CLIOUT <<EOF
-load NO-FILE.dat
-load ~/sandbox/hikecalc/data/catalina.dat
-EOF
-testCommand cli0_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+testCommand hc_help_1  "hc --help 2>&1"            "^\#" n
+testCommand hc_help_2  "hc shortest --help 2>&1"   "^\#" n
 
-cat > $CLIOUT <<EOF
-load ~/sandbox/hikecalc/data/catalina.dat
-th
-EOF
-testCommand cli2_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+# What trailheads are in the file?
+testCommand hc_th_0    "hc th $CD"                 "^\#" n
 
-cat > $CLIOUT <<EOF
-load ~/sandbox/hikecalc/data/catalina.dat
-wp
-EOF
-testCommand cli3_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+# What waypoints are in the file?
+testCommand hc_wp_0    "hc wp $CD"                 "^\#" n
 
-#!cat > $CLIOUT <<EOF
-#!load ~/sandbox/hikecalc/data/catalina.dat
-#!shortest SabinoTH HutchsPool
-#!EOF
-#!testCommand cli1_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
-cat > $CLIOUT <<EOF
-load ~/sandbox/hikecalc/data/catalina.dat
-shortest --details -w SabinoTH -w HutchsPool
-EOF
-testCommand cli1_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+# How far is it from Sabino visitor center to The Window?
+testCommand hc_short_0 "hc shortest -w SabinoTH -w TheWindow $CD"    "^\#" n
 
-cat > $CLIOUT <<EOF
-load ~/sandbox/hikecalc/data/catalina.dat
-dist dist.out
-EOF
-testCommand cli4_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
-sort dist.out > $DISTOUT
-testOutput  cli4_2 $DISTOUT '^\#' n
+# What is the detail distance from Butterfly to Palisades Ranger Station?
+cmd="hc shortest -w ButterflyTH -w PalisadesRangerTH --details $CD"
+testCommand hc_short_1 "$cmd"                             "^\#" n
+
+# Basic multi-day route
+cmd="hc shortest --details -w MarshallGulchTH -w SabinoTH $CD"
+testCommand hc_multi_0 "$cmd" "^\#" n
+# With Camps
+cmd="hc shortest --details -w MarshallGulchTH -w SabinoTH -c LemmonPoolJct 1 -c WestForkTE 2 -c BridalVeilFalls 3 $CD"
+testCommand hc_multi_1 "$cmd" "^\#" n
+
+#! cat > $CLIOUT <<EOF
+#! load NO-FILE.dat
+#! load ~/sandbox/hikecalc/data/catalina.dat
+#! EOF
+#! testCommand cli0_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+#!
+#! cat > $CLIOUT <<EOF
+#! load ~/sandbox/hikecalc/data/catalina.dat
+#! th
+#! EOF
+#! testCommand cli2_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+#!
+#! cat > $CLIOUT <<EOF
+#! load ~/sandbox/hikecalc/data/catalina.dat
+#! wp
+#! EOF
+#! testCommand cli3_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+#!
+#! #!cat > $CLIOUT <<EOF
+#! #!load ~/sandbox/hikecalc/data/catalina.dat
+#! #!shortest SabinoTH HutchsPool
+#! #!EOF
+#! #!testCommand cli1_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+#! cat > $CLIOUT <<EOF
+#! load ~/sandbox/hikecalc/data/catalina.dat
+#! shortest --details -w SabinoTH -w HutchsPool
+#! EOF
+#! testCommand cli1_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+#!
+#! cat > $CLIOUT <<EOF
+#! load ~/sandbox/hikecalc/data/catalina.dat
+#! dist dist.out
+#! EOF
+#! testCommand cli4_1 "hikecalc < $CLIOUT 2>&1" "^\#" n
+#! sort dist.out > $DISTOUT
+#! testOutput  cli4_2 $DISTOUT '^\#' n
 
 
 ###########################################
@@ -75,7 +102,6 @@ else
 fi
 
 ##############################################################################
-### Don't move or remove! 
+### Don't move or remove!
 cd $origdir
 exit $return_code
-
